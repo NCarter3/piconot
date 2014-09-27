4fc9d615cb337756236ff30a61e4628599bf890e
Emily Blatter <eblatter@hmc.edu>
2014-09-27 15:23:51 -0700

    NOW everything is functional. our language is even prettier than originally planned. the gods of coding must be smiling down upon us. the sun is shining and everything is beautiful. there are clouds in the sky. one looks like an elephant.

--- src/main/scala/piconot/Empty.scala
+++ src/main/scala/piconot/Empty.scala
@@ -3,7 +3,7 @@ import scalafx.application.JFXApp
 
 object Empty extends JFXApp with ruleClass {
   Proof. Begin ("empty.txt"). Consider (
-        0 - n -> 0 * n,
+        0 - n -> 0 - n,
 		0 + n - e - w -> 1 + w,
 		0 + n - e + w -> 2 - e,
 		0 + n + e -> 1,
